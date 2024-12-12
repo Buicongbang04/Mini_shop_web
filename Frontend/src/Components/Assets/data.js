7e2ea93286ee2a5d45cd221a47859b806fb65ed5
Buicongbang04 <buicongbang1010@gmail.com>
2024-12-12 17:29:45 +0700

    code

---
+++ Frontend/src/Components/Assets/data.js
@@ -0,0 +1,37 @@
+import p1_img from "../Assets/product_1.jpg";
+import p2_img from "../Assets/product_2.jpg";
+import p3_img from "../Assets/product_3.jpg";
+import p4_img from "../Assets/product_4.jpg";
+
+let data_product = [
+    {
+        id: 1,
+        name: "Product 1",
+        image: p1_img,
+        new_price: 100,
+        old_price: 120
+    },
+    {
+        id: 2,
+        name: "Product 2",
+        image: p2_img,
+        new_price: 200,
+        old_price: 220
+    },
+    {
+        id: 3,
+        name: "Product 3",
+        image: p3_img,
+        new_price: 300,
+        old_price: 320
+    },
+    {
+        id: 4,
+        name: "Product 4",
+        image: p4_img,
+        new_price: 400,
+        old_price: 420
+    }
+];
+
+export default data_product;
---
+++ Frontend/src/Components/Assets/new_collections.js
@@ -0,0 +1,102 @@
+import p1 from './new_product_1.jpg';
+import p2 from './new_product_2.jpg';
+import p3 from './new_product_3.jpg';
+import p4 from './new_product_4.jpg';
+import p5 from './new_product_5.jpg';
+import p6 from './new_product_6.jpg';
+import p7 from './new_product_7.jpg';
+import p8 from './new_product_8.jpg';
+import p9 from './new_product_9.jpg';
+import p10 from './new_product_10.jpg';
+import p11 from './new_product_11.jpg';
+import p12 from './new_product_12.jpg';
+
+
+let new_collections = [
+    {
+        id: 1,
+        name: "Product 1",
+        image: p1,
+        new_price: 100,
+        old_price: 120
+    },
+    {
+        id: 2,
+        name: "Product 2",
+        image: p2,
+        new_price: 200,
+        old_price: 220
+    },
+    {
+        id: 3,
+        name: "Product 3",
+        image: p3,
+        new_price: 300,
+        old_price: 320
+    },
+    {
+        id: 4,
+        name: "Product 4",
+        image: p4,
+        new_price: 400,
+        old_price: 420
+    },
+    {
+        id: 5,
+        name: "Product 5",
+        image: p5,
+        new_price: 500,
+        old_price: 520
+    },
+    {
+        id: 6,
+        name: "Product 6",
+        image: p6,
+        new_price: 600,
+        old_price: 620
+    },
+    {
+        id: 7,
+        name: "Product 7",
+        image: p7,
+        new_price: 700,
+        old_price: 720
+    },
+    {
+        id: 8,
+        name: "Product 8",
+        image: p8,
+        new_price: 800,
+        old_price: 820
+    },
+    {
+        id: 9,
+        name: "Product 9",
+        image: p9,
+        new_price: 900,
+        old_price: 920
+    },
+    {
+        id: 10,
+        name: "Product 10",
+        image: p10,
+        new_price: 1000,
+        old_price: 1020
+    },
+    {
+        id: 11,
+        name: "Product 11",
+        image: p11,
+        new_price: 1100,
+        old_price: 1120
+    },
+    {
+        id: 12,
+        name: "Product 12",
+        image: p12,
+        new_price: 1200,
+        old_price: 1220
+    }
+];
+
+export default new_collections;
