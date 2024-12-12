47985afc88cc1c36035cbfe9476d1175d35fab2c
Buicongbang04 <buicongbang1010@gmail.com>
2024-12-12 19:23:06 +0700

    code

---
+++ Frontend/src/Components/Assets/all_products.js
@@ -0,0 +1,329 @@
+import p1 from './product_1.jpg';
+import p2 from './product_2.jpg';
+import p3 from './product_3.jpg';
+import p4 from './product_4.jpg';
+import p5 from './product_5.jpg';
+import p6 from './product_6.jpg';
+import p7 from './product_7.jpg';
+import p8 from './product_8.jpg';
+import p9 from './product_9.jpg';
+import p10 from './product_10.jpg';
+import p11 from './product_11.jpg';
+import p12 from './product_12.jpg';
+import p13 from './product_13.jpg';
+import p14 from './product_14.jpg';
+import p15 from './product_15.jpg';
+import p16 from './product_16.jpg';
+import p17 from './product_17.jpg';
+import p18 from './product_18.jpg';
+import p19 from './product_19.jpg';
+import p20 from './product_20.jpg';
+import p21 from './product_21.jpg';
+import p22 from './product_22.jpg';
+import p23 from './product_23.jpg';
+import p24 from './product_24.jpg';
+import p25 from './product_25.jpg';
+import p26 from './product_26.jpg';
+import p27 from './product_27.jpg';
+import p28 from './product_28.jpg';
+import p29 from './product_29.jpg';
+import p30 from './product_30.jpg';
+import p31 from './product_31.jpg';
+import p32 from './product_32.jpg';
+import p33 from './product_33.jpg';
+import p34 from './product_34.jpg';
+import p35 from './product_35.jpg';
+import p36 from './product_36.jpg';
+
+const all_products = [
+    {
+        id: 1,
+        name: 'Product 1',
+        category: 'men',
+        image: p1,
+        new_price: 100,
+        old_price: 120,
+    },
+    {
+        id: 2,
+        name: 'Product 2',
+        category: 'men',
+        image: p2,
+        new_price: 200,
+        old_price: 220,
+    },
+    {
+        id: 3,
+        name: 'Product 3',
+        category: 'men',
+        image: p3,
+        new_price: 300,
+        old_price: 320,
+    },
+    {
+        id: 4,
+        name: 'Product 4',
+        category: 'men',
+        image: p4,
+        new_price: 400,
+        old_price: 420,
+    },
+    {
+        id: 5,
+        name: 'Product 5',
+        category: 'men',
+        image: p5,
+        new_price: 500,
+        old_price: 520,
+    },
+    {
+        id: 6,
+        name: 'Product 6',
+        category: 'men',
+        image: p6,
+        new_price: 600,
+        old_price: 620,
+    },
+    {
+        id: 7,
+        name: 'Product 7',
+        category: 'men',
+        image: p7,
+        new_price: 700,
+        old_price: 720,
+    },
+    {
+        id: 8,
+        name: 'Product 8',
+        category: 'men',
+        image: p8,
+        new_price: 800,
+        old_price: 820,
+    },
+    {
+        id: 9,
+        name: 'Product 9',
+        category: 'men',
+        image: p9,
+        new_price: 900,
+        old_price: 920,
+    },
+    {
+        id: 10,
+        name: 'Product 10',
+        category: 'men',
+        image: p10,
+        new_price: 1000,
+        old_price: 1020,
+    },
+    {
+        id: 11,
+        name: 'Product 11',
+        category: 'men',
+        image: p11,
+        new_price: 1100,
+        old_price: 1120,
+    },
+    {
+        id: 12,
+        name: 'Product 12',
+        category: 'men',
+        image: p12,
+        new_price: 1200,
+        old_price: 1220,
+    },
+    {
+        id: 13,
+        name: 'Product 13',
+        category: 'women',
+        image: p13,
+        new_price: 1300,
+        old_price: 1320,
+    },
+    {
+        id: 14,
+        name: 'Product 14',
+        category: 'women',
+        image: p14,
+        new_price: 1400,
+        old_price: 1420,
+    },
+    {
+        id: 15,
+        name: 'Product 15',
+        category: 'women',
+        image: p15,
+        new_price: 1500,
+        old_price: 1520,
+    },
+    {
+        id: 16,
+        name: 'Product 16',
+        category: 'women',
+        image: p16,
+        new_price: 1600,
+        old_price: 1620,
+    },
+    {
+        id: 17,
+        name: 'Product 17',
+        category: 'women',
+        image: p17,
+        new_price: 1700,
+        old_price: 1720,
+    },
+    {
+        id: 18,
+        name: 'Product 18',
+        category: 'women',
+        image: p18,
+        new_price: 1800,
+        old_price: 1820,
+    },
+    {
+        id: 19,
+        name: 'Product 19',
+        category: 'women',
+        image: p19,
+        new_price: 1900,
+        old_price: 1920,
+    },
+    {
+        id: 20,
+        name: 'Product 20',
+        category: 'women',
+        image: p20,
+        new_price: 2000,
+        old_price: 2020,
+    },
+    {
+        id: 21,
+        name: 'Product 21',
+        category: 'women',
+        image: p21,
+        new_price: 2100,
+        old_price: 2120,
+    },
+    {
+        id: 22,
+        name: 'Product 22',
+        category: 'women',
+        image: p22,
+        new_price: 2200,
+        old_price: 2220,
+    },
+    {
+        id: 23,
+        name: 'Product 23',
+        category: 'women',
+        image: p23,
+        new_price: 2300,
+        old_price: 2320,
+    },
+    {
+        id: 24,
+        name: 'Product 24',
+        category: 'women',
+        image: p24,
+        new_price: 2400,
+        old_price: 2420,
+    },
+    {
+        id: 25,
+        name: 'Product 25',
+        category: 'kid',
+        image: p25,
+        new_price: 2500,
+        old_price: 2520,
+    },
+    {
+        id: 26,
+        name: 'Product 26',
+        category: 'kid',
+        image: p26,
+        new_price: 2600,
+        old_price: 2620,
+    },
+    {
+        id: 27,
+        name: 'Product 27',
+        category: 'kid',
+        image: p27,
+        new_price: 2700,
+        old_price: 2720,
+    },
+    {
+        id: 28,
+        name: 'Product 28',
+        category: 'kid',
+        image: p28,
+        new_price: 2800,
+        old_price: 2820,
+    },
+    {
+        id: 29,
+        name: 'Product 29',
+        category: 'kid',
+        image: p29,
+        new_price: 2900,
+        old_price: 2920,
+    },
+    {
+        id: 30,
+        name: 'Product 30',
+        category: 'kid',
+        image: p30,
+        new_price: 3000,
+        old_price: 3020,
+    },
+    {
+        id: 31,
+        name: 'Product 31',
+        category: 'kid',
+        image: p31,
+        new_price: 3100,
+        old_price: 3120,
+    },
+    {
+        id: 32,
+        name: 'Product 32',
+        category: 'kid',
+        image: p32,
+        new_price: 3200,
+        old_price: 3220,
+    },
+    {
+        id: 33,
+        name: 'Product 33',
+        category: 'kid',
+        image: p33,
+        new_price: 3300,
+        old_price: 3320,
+    },
+    {
+        id: 34,
+        name: 'Product 34',
+        category: 'kid',
+        image: p34,
+        new_price: 3400,
+        old_price: 3420,
+    },
+    {
+        id: 35,
+        name: 'Product 35',
+        category: 'kid',
+        image: p35,
+        new_price: 3500,
+        old_price: 3520,
+    },
+    {
+        id: 36,
+        name: 'Product 36',
+        category: 'kid',
+        image: p36,
+        new_price: 3600,
+        old_price: 3620,
+    },
+];
+
+export default all_products;
